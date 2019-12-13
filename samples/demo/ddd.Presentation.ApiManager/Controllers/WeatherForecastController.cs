ad6928908b5caa193a7ab63f4183a14a858c7291
kant <zhouzixing@yunepress.com>
2019-12-13 16:22:04 +0800

    add solution files

---
+++ samples/demo/ddd.Presentation.ApiManager/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ddd.Infrastructure.MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ddd.Presentation.ApiManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly IMediator _mediator;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger
+            , IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _mediator.Publish(new Pinged());
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ samples/demo/ddd.Presentation.ApiManager/Pinged.cs
@@ -0,0 +1,29 @@
+﻿using ddd.Infrastructure.MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ddd.Presentation.ApiManager
+{
+    public class Pinged:INotification
+    {
+    }
+
+
+    public class PingedHandler : INotificationHandler<Pinged>
+    {
+        private readonly ILogger<PingedHandler> _logger;
+        public PingedHandler(ILogger<PingedHandler> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Handle(Pinged notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("hallo");
+            await Unit.Task;
+        }
+    }
+}
